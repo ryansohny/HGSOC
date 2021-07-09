83d79d3e5de635b32991a2d12c87fce6b7f6339c
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2021-07-10 08:38:24 +0900

    Update _EMT-Index_HRDscore.R

--- RNA-seq/_EMT-Index_HRDscore.R
+++ RNA-seq/_EMT-Index_HRDscore.R
@@ -6,6 +6,6 @@ dat <- read.csv("EMT_HRD.csv", header=TRUE, row.names=1)
 
 # Draw Scatterplot
 pdf("EMT-index_HRD_scatterplot.pdf")
-ggscatter(dat, x="EMT.index", y="HRD", add="reg.line", conf.int=TRUE, cor.coef=TRUE, cor.method="spearman", xlab="EMT index", ylab="HRD score")
+ggscatter(dat, x="EMT.index", y="HRDsum", add="reg.line", conf.int=TRUE, cor.coef=TRUE, cor.method="spearman", xlab="EMT index", ylab="HRD score")
 dev.off()
 
