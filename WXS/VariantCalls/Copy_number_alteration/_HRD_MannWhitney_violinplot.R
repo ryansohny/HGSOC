62785278b0c691b620f3f96c0fcf75bb91b9eb48
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2021-07-10 00:57:32 +0900

    Update _HRD_MannWhitney_violinplot.R

--- WXS/VariantCalls/Copy_number_alteration/_HRD_MannWhitney_violinplot.R
+++ WXS/VariantCalls/Copy_number_alteration/_HRD_MannWhitney_violinplot.R
@@ -5,9 +5,25 @@ library(rstatix)
 dat <- read.csv("HRD_score.csv", row.names=1)
 
 dat %>% wilcox_test(HRD ~ Cluster, exact=TRUE, detailed=TRUE, p.adjust.method="bonferroni")
+## A tibble: 1 x 12
+#  estimate .y.   group1 group2    n1    n2 statistic       p conf.low conf.high
+#*    <dbl> <chr> <chr>  <chr>  <int> <int>     <dbl>   <dbl>    <dbl>     <dbl>
+#1     14.0 HRD   A      B         15     5      72.5 0.00256     7.00      18.0
 dat %>% wilcox_test(Telomeric.AI ~ Cluster, exact=TRUE, detailed=TRUE, p.adjust.method="bonferroni")
+## A tibble: 1 x 12
+#  estimate .y.   group1 group2    n1    n2 statistic      p conf.low conf.high
+#*    <dbl> <chr> <chr>  <chr>  <int> <int>     <dbl>  <dbl>    <dbl>     <dbl>
+#1     17.0 Telo… A      B         15     5        61 0.0444     2.00      24.0
 dat %>% wilcox_test(LST ~ Cluster, exact=TRUE, detailed=TRUE, p.adjust.method="bonferroni")
+## A tibble: 1 x 12
+#  estimate .y.   group1 group2    n1    n2 statistic     p conf.low conf.high
+#*    <dbl> <chr> <chr>  <chr>  <int> <int>     <dbl> <dbl>    <dbl>     <dbl>
+#1     21.0 LST   A      B         15     5        66 0.014     2.00      27.0
 dat %>% wilcox_test(HRD.sum ~ Cluster, exact=TRUE, detailed=TRUE, p.adjust.method="bonferroni")
+## A tibble: 1 x 12
+#  estimate .y.   group1 group2    n1    n2 statistic      p conf.low conf.high
+#*    <dbl> <chr> <chr>  <chr>  <int> <int>     <dbl>  <dbl>    <dbl>     <dbl>
+#1     51.0 HRD.… A      B         15     5        67 0.0113     11.0      70.0
 
 pdf("scarHRD_violoinplot.pdf")
 p1 <- ggplot(dat, aes(x=Cluster, y=HRD, fill=Cluster)) + geom_violin(trim=TRUE) + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.7) + scale_fill_manual(values=c('#000066','#990000'))  + theme_classic()
