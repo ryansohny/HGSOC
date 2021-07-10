836f3636639de82059a85958c0c5807720ffbaae
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2021-07-10 10:55:33 +0900

    Update _HRDscore_boxplot.R

--- RNA-seq/_HRDscore_boxplot.R
+++ RNA-seq/_HRDscore_boxplot.R
@@ -14,11 +14,16 @@ png("HRDscore_byCluster.png")
 
 pdf("scarHRD_violoinplot.pdf")
 p1 <- ggplot(dat, aes(x=Cluster, y=HRD, fill=Cluster)) + geom_violin(trim=TRUE) + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.7) + scale_fill_manual(values=c('#000066','#990000'))  + theme_classic()
-p2 <- ggplot(dat, aes(x=Cluster, y=Telomeric.AI, fill=Cluster)) + geom_violin(trim=TRUE) + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.7) + scale_fill_manual(values=c('#000066','#990000'))  + theme_classic()
+p2 <- ggplot(dat, aes(x=Cluster, y=TelomericAI, fill=Cluster)) + geom_violin(trim=TRUE) + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.7) + scale_fill_manual(values=c('#000066','#990000'))  + theme_classic()
 p3 <- ggplot(dat, aes(x=Cluster, y=LST, fill=Cluster)) + geom_violin(trim=TRUE) + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.7) + scale_fill_manual(values=c('#000066','#990000'))  + theme_classic()
-p4 <- ggplot(dat, aes(x=Cluster, y=HRD.sum, fill=Cluster)) + geom_violin(trim=TRUE) + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.7) + scale_fill_manual(values=c('#000066','#990000'))  + theme_classic()
+p4 <- ggplot(dat, aes(x=Cluster, y=HRDsum, fill=Cluster)) + geom_violin(trim=TRUE) + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.7) + scale_fill_manual(values=c('#000066','#990000'))  + theme_classic()
 
 ggarrange(p1, p2, p3, p4, labels=c("A", "B", "C", "D"), ncol=2, nrow=2)
 dev.off()
 
-#dat %>% wilcox_test(Stromal ~ Cluster, exact=TRUE, detailed=TRUE, p.adjust.method="bonferroni")
+dat %>% wilcox_test(HRD ~ Cluster, exact=TRUE, detailed=TRUE, p.adjust.method="bonferroni")
+dat %>% wilcox_test(TelomericAI ~ Cluster, exact=TRUE, detailed=TRUE, p.adjust.method="bonferroni")
+dat %>% wilcox_test(LST ~ Cluster, exact=TRUE, detailed=TRUE, p.adjust.method="bonferroni")
+dat %>% wilcox_test(HRDsum ~ Cluster, exact=TRUE, detailed=TRUE, p.adjust.method="bonferroni")
+
+
